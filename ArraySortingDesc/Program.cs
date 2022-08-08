6fe8328546c1ba36e53f7b71362d6ce34b110706
Oluwaseyi0910 <110403016+Oluwaseyi0910@users.noreply.github.com>
2022-08-08 08:51:16 +0100

    Add project files.

---
+++ ArraySortingDesc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArraySortingDesc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraySeyi = { 15, 34, 25, 17, 42, 57, 77, 52, 27, 68 };
+
+            int x = arraySeyi.Length;
+
+
+            for (int i = 0; i < x-1; i++)
+            {
+                if (arraySeyi[i] < arraySeyi[i+1])
+                {
+                    int placeHolder = arraySeyi [i];
+                    arraySeyi [i] = arraySeyi [i + 1];
+                    arraySeyi [i + 1] = placeHolder;
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(string.Join(",", arraySeyi));
+        }
+    }
+}
